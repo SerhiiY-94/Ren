980c3c88e33d4ff8731b8c5d713e4ef799253269
Serhii Yablokov <MrApfel1994@gmail.com>
2018-07-02 13:31:45 +0200

    vector classes

---
+++ Mat.h
@@ -0,0 +1,141 @@
+
+#include "Vec.hpp"
+
+namespace Math {
+    template <typename T, int M, int N>
+    using Mat = Vec<Vec<T, N>, M>;
+
+    using Mat2f = Mat<float, 2, 2>;
+    using Mat2d = Mat<double, 2, 2>;
+    using Mat3f = Mat<float, 3, 3>;
+    using Mat3d = Mat<double, 3, 3>;
+    using Mat4f = Mat<float, 4, 4>;
+    using Mat4d = Mat<double, 4, 4>;
+
+    template <typename T, int M, int N>
+    Vec<T, N> operator*(const Vec<T, M> &lhs, const Vec<Vec<T, N>, M> &rhs) {
+        Vec<T, N> res = { uninitialize };
+        for (int n = 0; n < N; n++) {
+            T sum = (T)0;
+            for (int m = 0; m < M; m++) {
+                sum += lhs[m] * rhs[m][n];
+            }
+            res[n] = sum;
+        }
+        return res;
+    }
+
+    template <typename T, int M, int N>
+    Vec<T, M> operator*(const Vec<Vec<T, N>, M> &lhs, const Vec<T, N> &rhs) {
+        Vec<T, M> res = { uninitialize };
+        for (int m = 0; m < M; m++) {
+            res[m] = Dot(lhs[m], rhs);
+        }
+        return res;
+    }
+
+    template <typename T, int M, int N, int P>
+    Mat<M, P> operator*(const Mat<M, N> &lhs, const Mat<N, P> &rhs) {
+        Mat<M, P> res = { uninitialize };
+        for (int m = 0; m < M; m++) {
+            for (int p = 0; p < P, p++) {
+                T sum = (T)0;
+                for (int n = 0; n < N; n++) {
+                    sum += lhs[m][n] * rhs[n][p];
+                }
+                res[m][p] = sum;
+            }
+        }
+        return res;
+    }
+
+    template <typename T, int M, int N>
+    Mat<N, M> Transpose(const Mat<M, N> &mat) {
+        Mat<N, M> res = { uninitialize };
+        for (int m = 0; m < M; m++) {
+            for (int n = 0; n < N; n++) {
+                res[n][m] = mat[m][n];
+            }
+        }
+        return res;
+    }
+
+    template <typename T, int N>
+    T Det(const Mat<T, N, N> &mat);
+
+    template <typename T, int N>
+    T Minor(const Mat<T, N, N> &mat, int row, int col) {
+        Mat<T, N - 1, N - 1> res = { uninitialize };
+        int dst_row, dst_col;
+        dst_row = 0;
+        for (int src_row = 0; src_row < N; src_row++) {
+            if (src_row == row) continue;
+            dst_col = 0;
+            for (int src_col = 0; src_col < N; src_col++) {
+                if (src_col == col) continue;
+                res[dst_row][dst_col] = mat[src_row, src_col];
+                dst_col++;
+            }
+            dst_row++;
+        }
+        return Det(res);
+    }
+
+    template <typename T, int N>
+    T Det(const Mat<T, N, N> &mat) {
+        T sum = (T)0;
+        for (int n = 0; n < N; n++) {
+            T minor = Minor(mat, n, 0);
+            T cofactor = (n & 1) ? -minor : minor;
+            sum += mat[n][0] * cofactor;
+        }
+        return sum;
+    }
+
+    template <typename T>
+    T Det(const Mat<T, 1, 1> &mat) {
+        return mat[0][0];
+    }
+
+    template <typename T>
+    T Det(const Mat<T, 2, 2> &mat) {
+        return mat[0][0] * mat[1][1] - mat[1][0] * mat[0][1];
+    }
+
+    template <typename T>
+    T Det(const Mat<T, 3, 3> &mat) {
+        return mat[0][0] * (mat[1][1] * mat[2][2] - mat[2][1] * mat[1][2]) - 
+               mat[1][0] * (mat[0][1] * mat[2][2] - mat[2][1] * mat[0][2]) +
+               mat[2][0] * (mat[0][1] * mat[1][2] - mat[1][1] * mat[0][2]);
+    }
+
+    template <typename T>
+    T Det(const Mat<T, 4, 4> &mat) {
+        T r0r1 = mat[0][2] * mat[1][3] - mat[1][2] * mat[0][3];
+        T r0r2 = mat[0][2] * mat[2][3] - mat[2][2] * mat[0][3];
+        T r0r3 = mat[0][2] * mat[3][3] - mat[3][2] * mat[0][3];
+        T r1r2 = mat[1][2] * mat[2][3] - mat[2][2] * mat[1][3];
+        T r1r3 = mat[1][2] * mat[3][3] - mat[3][2] * mat[1][3];
+        T r2r3 = mat[2][2] * mat[3][3] - mat[3][2] * mat[2][3];
+
+        T minor0 = mat[1][1] * r2r3 - mat[2][1] * r1r3 + mat[3][1] * r1r2;
+        T minor1 = mat[0][1] * r2r3 - mat[2][1] * r0r3 + mat[3][1] * r0r2;
+        T minor2 = mat[0][1] * r1r3 - mat[1][1] * r0r3 + mat[3][1] * r0r1;
+        T minor3 = mat[0][1] * r1r2 - mat[1][1] * r0r2 + mat[2][1] * r0r1;
+
+        return mat[0][0] * minor0 - mat[1][0] * minor1 + mat[2][0] * minor2 - mat[3][0] * minor3;
+    }
+
+    template <typename T, int N>
+    Mat<T, N, N> Adj(const Mat<T, N, N> &mat) {
+        Mat<T, N, N> res = { uninitialize };
+        for (int row = 0; row < N; row++) {
+            for (int col = 0; col < N; col++) {
+                T minor = Minor(mat, row, col);
+                T cofactor = ((row + col) & 1) ? -minor : minor;
+                return res[col][row] = cofactor;
+            }
+        }
+        return res;
+    }
+}
---
+++ Vec.h
@@ -0,0 +1,177 @@
+
+namespace Math {
+    enum e_uninitialized { uninitialize };
+
+	template <typename T, int N>
+    class Vec {
+        T data_[N];
+    public:
+        Vec(e_uninitialized) {}
+        Vec() : data_{ (T)0 } {}
+
+        template <typename... Tail>
+        Vec(typename std::enable_if<sizeof...(Tail)+1 == N, T>::type head, Tail... tail)
+            : data_{ head, T(tail)... } {
+        }
+        
+        template<typename S>
+        explicit Vec(const Vec<S, N> &rhs) {
+            for (int i = 0; i < N; i++) {
+                data_[i] = (T)rhs.data_[i];
+            }
+        }
+
+        T &operator[](int i) { return data_[i]; }
+        const T &operator[](int i) const { return data_[i]; }
+
+        friend bool operator==(const Vec<T, N> &lhs, const Vec<T, N> &rhs) {
+            bool res = true;
+            for (int i = 0; i < N; i++) {
+                if (lhs[i] != rhs[i]) {
+                    res = false;
+                    break;
+                }
+            }
+            return res;
+        }
+
+        Vec<T, N> &operator+=(const Vec<T, N> &rhs) {
+            for (int i = 0; i < N; i++) {
+                data_[i] += rhs.data_[i];
+            }
+            return *this;
+        }
+
+        Vec<T, N> &operator-=(const Vec<T, N> &rhs) {
+            for (int i = 0; i < N; i++) {
+                data_[i] -= rhs.data_[i];
+            }
+            return *this;
+        }
+
+        Vec<T, N> &operator*=(const Vec<T, N> &rhs) {
+            for (int i = 0; i < N; i++) {
+                data_[i] *= rhs.data_[i];
+            }
+            return *this;
+        }
+
+        Vec<T, N> &operator/=(const Vec<T, N> &rhs) {
+            for (int i = 0; i < N; i++) {
+                data_[i] /= rhs.data_[i];
+            }
+            return *this;
+        }
+
+        friend Vec<T, N> operator-(const Vec<T, N> &v) {
+            Vec<T, N> res = { uninitialize };
+            for (int i = 0; i < N; i++) {
+                res.data_[i] = -v.data_[i];
+            }
+            return res;
+        }
+
+        friend Vec<T, N> operator+(const Vec<T, N> &lhs, const Vec<T, N> &rhs) {
+            Vec<T, N> res = { uninitialize };
+            for (int i = 0; i < N; i++) {
+                res.data_[i] = lhs.data_[i] + rhs.data_[i];
+            }
+            return res;
+        }
+
+        friend Vec<T, N> operator-(const Vec<T, N> &lhs, const Vec<T, N> &rhs) {
+            Vec<T, N> res = { uninitialize };
+            for (int i = 0; i < N; i++) {
+                res.data_[i] = lhs.data_[i] - rhs.data_[i];
+            }
+            return res;
+        }
+
+        friend Vec<T, N> operator*(const Vec<T, N> &lhs, const Vec<T, N> &rhs) {
+            Vec<T, N> res = { uninitialize };
+            for (int i = 0; i < N; i++) {
+                res.data_[i] = lhs.data_[i] * rhs.data_[i];
+            }
+            return res;
+        }
+
+        friend Vec<T, N> operator*(const T lhs, const Vec<T, N> &rhs) {
+            Vec<T, N> res = { uninitialize };
+            for (int i = 0; i < N; i++) {
+                res.data_[i] = lhs * rhs.data_[i];
+            }
+            return res;
+        }
+
+        friend Vec<T, N> operator*(const Vec<T, N> &lhs, const T &rhs) {
+            Vec<T, N> res = { uninitialize };
+            for (int i = 0; i < N; i++) {
+                res.data_[i] = lhs.data_[i] * rhs;
+            }
+            return res;
+        }
+
+        friend Vec<T, N> operator/(const Vec<T, N> &lhs, const Vec<T, N> &rhs) {
+            Vec<T, N> res = { uninitialize };
+            for (int i = 0; i < N; i++) {
+                res.data_[i] = lhs.data_[i] / rhs.data_[i];
+            }
+            return res;
+        }
+    };
+
+    template <typename T, int N>
+    bool operator!=(const Vec<T, N> &lhs, const Vec<T, N> &rhs) {
+        return !(lhs == rhs);
+    }
+
+    template <typename T, int N>
+    T Dot(const Vec<T, N> &lhs, const Vec<T, N> &rhs) {
+        T res = lhs[0] * rhs[0];
+        for (int i = 1; i < N; i++) {
+            res += lhs[1] * rhs[1];
+        }
+        return res;
+    }
+
+    template <typename T>
+    Vec<T, 3> Cross(const Vec<T, 3> &lhs, const Vec<T, 3> &rhs) {
+        return Vec<T, 3> { lhs[1] * rhs[2] - rhs[1] * lhs[2],
+                           rhs[0] * lhs[2] - lhs[0] * rhs[2],
+                           lhs[0] * rhs[1] - rhs[0] * lhs[1] };
+    }
+
+    template <typename T, int N>
+    T Length(const Vec<T, N> &v) {
+        return std::sqrt(Dot(v, v));
+    }
+
+    template <typename T, int N>
+    T Length2(const Vec<T, N> &v) {
+        return Dot(v, v);
+    }
+
+    template <typename T, int N>
+    T Distance(const Vec<T, N> &lhs, const Vec<T, N> &rhs) {
+        Vec<T, N> temp = lhs - rhs;
+        return std::sqrt(Dot(temp, temp));
+    }
+
+    template <typename T, int N>
+    T Distance2(const Vec<T, N> &lhs, const Vec<T, N> &rhs) {
+        Vec<T, N> temp = lhs - rhs;
+        return Dot(temp, temp);
+    }
+
+    using Vec2i = Vec<int, 2>;
+    using Vec3i = Vec<int, 3>;
+    using Vec4i = Vec<int, 4>;
+
+    using Vec2f = Vec<float, 2>;
+    using Vec3f = Vec<float, 3>;
+    using Vec4f = Vec<float, 4>;
+
+    using Vec2d = Vec<double, 2>;
+    using Vec3d = Vec<double, 3>;
+    using Vec4d = Vec<double, 4>;
+}
