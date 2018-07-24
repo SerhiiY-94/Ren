f81833adb01c285a6b7e99c02b74f1d8a7d416e5
Serhii Yablokov <MrApfel1994@gmail.com>
2018-07-24 15:40:21 +0200

    cont.

--- tests/test_programSW.cpp
+++ tests/test_programSW.cpp
@@ -6,10 +6,10 @@
 #include "../Program.h"
 #include "../SW/SW.h"
 
-class ProgramTest : public ren::Context {
+class ProgramTest : public Ren::Context {
 public:
     ProgramTest() {
-        ren::Context::Init(1, 1);
+        Ren::Context::Init(1, 1);
     }
 };
 
@@ -22,16 +22,16 @@ void test_program() {
         // Create program
         ProgramTest test;
 
-        ren::eProgLoadStatus status;
-        ren::Attribute _attrs[] = { {} };
-        ren::Uniform _unifs[] = { {} };
-        ren::ProgramRef p = test.LoadProgramSW("constant", nullptr, nullptr, 0, _attrs, _unifs, &status);
+        Ren::eProgLoadStatus status;
+        Ren::Attribute _attrs[] = { {} };
+        Ren::Uniform _unifs[] = { {} };
+        Ren::ProgramRef p = test.LoadProgramSW("constant", nullptr, nullptr, 0, _attrs, _unifs, &status);
 
         require(p);
-        require(status == ren::ProgSetToDefault);
+        require(status == Ren::ProgSetToDefault);
 
         {
-            ren::Program *pp = p.get();
+            Ren::Program *pp = p.get();
 
             require(pp != nullptr);
             require(std::string(pp->name()) == "constant");
@@ -39,14 +39,14 @@ void test_program() {
             require(!pp->ready());
         }
 
-        ren::Uniform unifs[] = { {"unif1", 0, SW_FLOAT, 1}, {"unif2", 1, SW_VEC3, 1}, {} };
-        ren::Attribute attrs[] = { {"attr1", 0, -1, 1}, {"attr2", 1, -1, 1}, {} };
+        Ren::Uniform unifs[] = { {"unif1", 0, SW_FLOAT, 1}, {"unif2", 1, SW_VEC3, 1}, {} };
+        Ren::Attribute attrs[] = { {"attr1", 0, -1, 1}, {"attr2", 1, -1, 1}, {} };
 
         test.LoadProgramSW("constant", (void*)vshader, (void*)fshader, 0, attrs, unifs, &status);
 
-        require(status == ren::ProgCreatedFromData);
+        require(status == Ren::ProgCreatedFromData);
 
-        ren::Program *pp = p.get();
+        Ren::Program *pp = p.get();
 
         require(pp != nullptr);
         require(std::string(pp->name()) == "constant");
